1e8ecd49cbae56668347e6d13e71f9e6d740dc12
de <lishangde228@163.com>
2024-05-10 22:19:31 +0800

    苍穹外卖项目初始代码

--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 /**
  * 自定义切面，实现公共字段自动填充处理逻辑
  * (切面所在的类叫切面类,注解 @Aspect)
- * 切面 = 切入点表达式 + 通知
+ * (切面 = 切入点表达式 + 通知)
  */
 @Aspect
 @Component
@@ -19,12 +19,19 @@ public class AutoFillAspect {
 
     /**
      * 切入点
+     * (**抽取**)
+     * (Spring提供了 @PointCut 注解，该注解的作用是将公共的切入点表达式抽取出来，需要用到时引用该切入点表达式即可。)
      */
     @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
     public void autoFillPointCut(){}
 
     /**
      * 前置通知，在通知中进行公共字段的赋值
+     * (在Spring中用JoinPoint抽象了连接点，用它可以获得方法执行时的相关信息，如目标类名、方法名、方法参数等。)
+     *
+     * (对于@Around通知，获取连接点信息只能使用ProceedingJoinPoint类型)
+     *
+     * (对于其他四种通知，获取连接点信息只能使用JoinPoint，它是ProceedingJoinPoint的父类型)
      */
     @Before("autoFillPointCut()")
     public void autoFill(JoinPoint joinPoint){
